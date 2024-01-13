5270787077dc2978189a2e291ca97718ede22e8c
Sai Aryan Goswami <saiaryan.goswami1784@gmail.com>
2024-01-13 20:25:17 +0530

    Added greeting and also trademark

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ const url = "https://api.dictionaryapi.dev/api/v2/entries/en/"
 const result = document.getElementById("result");
 const sound = document.getElementById("sound")
 const btn = document.getElementById("search-btn")
+const N = "NULL"
 
-btn.addEventListener("click", () => {
+btn.addEventListener("click", (err) => {
     let inpword = document.getElementById("inp-word").value
     fetch(`${url}${inpword}`)
     .then((response) => response.json())
@@ -18,7 +19,7 @@ btn.addEventListener("click", () => {
             </button>
         </div>
         <div class="details">
-            <p>${data[0].meanings[0].partOfSpeech}</p>
+            <p>${data[0].meanings[0].partOfSpeech ? data[0].meanings[0].partOfSpeech : N}</p>
             <p>/${data[0].phonetic}/</p>
         </div>
         <p class="word-meaning">
@@ -36,3 +37,28 @@ btn.addEventListener("click", () => {
 function playSound() {
     sound.play();
 }
+
+let heading = document.getElementById("greeting")
+
+
+let today = new Date();
+let hourNow = today.getHours();
+let Greeting;
+
+if (hourNow > 18) {
+    Greeting = "Good Evening!";
+}
+
+else if (hourNow > 12) {
+    Greeting = "Good Afternoon!";
+}
+
+else if( hourNow > 0) {
+    Greeting = "Good Morning!";
+}
+
+else{
+    Greeting = "Welcome!";
+}
+
+heading.textContent = Greeting;
